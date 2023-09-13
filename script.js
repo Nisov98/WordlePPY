14e82d572a95d00e947a4cac6cd13ed9e5ce93ee
Nicolas Ojeda <142521936+Nisov98@users.noreply.github.com>
2023-09-13 06:14:47 -0400

    Juego

---
+++ script.js
@@ -0,0 +1,56 @@
+let intentos = 6;
+let palabra = "APPLE";
+
+window.addEventListener('load', init);
+
+function init() {
+  const button = document.getElementById("guess-button");
+  button.addEventListener("click", intentar);
+}
+
+function leerIntento() {
+  let intento = document.getElementById("guess-input");
+  intento = intento.value;
+  intento = intento.toUpperCase();
+  return intento;
+}
+
+function intentar() {
+  const INTENTO = leerIntento();
+  if (INTENTO === palabra) {
+    terminar("<h1>GANASTE!😀</h1>");
+    return;
+  }
+  const GRID = document.getElementById("grid");
+  const ROW = document.createElement('div');
+  ROW.className = 'row';
+  for (let i in palabra) {
+    const SPAN = document.createElement('span');
+    SPAN.className = 'letter';
+    if (INTENTO[i] === palabra[i]) {
+      SPAN.innerHTML = INTENTO[i];
+      SPAN.style.backgroundColor = '#79b851';
+    } else if (palabra.includes(INTENTO[i])) {
+      SPAN.innerHTML = INTENTO[i];
+      SPAN.style.backgroundColor = '#f3c237';
+    } else {
+      SPAN.innerHTML = INTENTO[i];
+      SPAN.style.backgroundColor = '#a4aec4';
+    }
+    ROW.appendChild(SPAN);
+  }
+  GRID.appendChild(ROW);
+  intentos--;
+  if (intentos === 0) {
+    terminar("<h1>PERDISTE!😖</h1>");
+  }
+}
+
+function terminar(mensaje) {
+  const INPUT = document.getElementById("guess-input");
+  INPUT.disabled = true;
+  const BOTON = document.getElementById("guess-button");
+  BOTON.disabled = true;
+  let contenedor = document.getElementById('guesses');
+  contenedor.innerHTML = mensaje;
+}
